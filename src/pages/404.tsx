215502d47e0324cfbd6694c619bea44f20743666
hisho <hisho.web@gmail.com>
2021-03-11 19:31:24 +0900

    💥 jsx to tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from 'gatsby';
+
+const IndexPage = () => {
+  return (
+    <main>
+      index.page
+      <Link to="/">
+        back index page
+      </Link>
+    </main>
+  )
+}
+
+export default IndexPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      index.page
+    </main>
+  )
+}
+
+export default IndexPage
