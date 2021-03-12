48a9a4f1447829571fff120508c0243cf9485255
hisho <hisho.web@gmail.com>
2021-03-12 11:44:52 +0900

    🎉 add template

---
+++ src/pages/template/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Layout, SEO} from "@src/layouts";
+import * as styles from "@src/pages/template/index.module.css";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO/>
+      <div className="wrapper">
+        <section>
+          <h2 className={styles.test}>css modules</h2>
+          <h2 className={styles.testTest}>css modules kebab to camelcase</h2>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
