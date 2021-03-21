7633b88a4a66637c47e6806fcf77fb6e7f70ccb2
hisho <hisho.web@gmail.com>
2021-03-21 23:13:16 +0900

    📦 add material-uo

--- src/pages/template/index.tsx
+++ src/pages/template/index.tsx
@@ -5,6 +5,11 @@ import { GatsbyImage, StaticImage } from 'gatsby-plugin-image';
 import { graphql, useStaticQuery } from 'gatsby';
 import { PageContext } from '@src/store';
 import { usePageReducer } from '@src/hooks';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@material-ui/core';
 
 const IndexPage: FCP = () => {
   const currentPage = usePageReducer('111111');
@@ -13,6 +18,15 @@ const IndexPage: FCP = () => {
     <PageContext.Provider value={currentPage}>
       <Layout>
         <SEO />
+        <Accordion>
+          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
+            アコーディオン
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
         <div className="wrapper">
           <section>
             <h2 className={styles.test}>css modules</h2>
