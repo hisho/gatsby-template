62cec2a174ec9c15632812775a490a9f325b680e
hisho <hisho.web@gmail.com>
2021-03-23 00:01:39 +0900

    🎉 add useVariables.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from '@src/hooks/useSiteMetaData';
 export * from '@src/hooks/usePage';
 export * from '@src/hooks/useSEO';
 export * from '@src/hooks/usePageReducer';
+export * from "@src/configs/variables";
