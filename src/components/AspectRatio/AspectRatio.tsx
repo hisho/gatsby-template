f3fa67983f34a8494f3ba0511bfbd6540eb4aa81
hisho <hisho.web@gmail.com>
2021-03-22 15:04:53 +0900

    🎉 add perccentage to helper

--- src/components/AspectRatio/AspectRatio.tsx
+++ src/components/AspectRatio/AspectRatio.tsx
@@ -1,20 +1,22 @@
-import React, { FCX } from 'react';
+import React, {FCX} from 'react';
+import {percentage} from "@src/helper";
 
 type AspectRatioPropsType = {
   width: number;
   height: number;
 };
 
-export const AspectRatio: FCX<AspectRatioPropsType> = ({
-  className = '',
-  children,
-  width,
-  height,
-}) => {
+export const AspectRatio: FCX<AspectRatioPropsType> = (
+  {
+    className = '',
+    children,
+    width,
+    height,
+  }) => {
   return (
     <div
       className={className}
-      style={{ paddingTop: `${(height / width) * 100}%` }}
+      style={{paddingTop: percentage(height / width)}}
     >
       {children}
     </div>
---
+++ src/helper/index.ts
@@ -0,0 +1,1 @@
+export {percentage} from "@src/helper/percentage";
