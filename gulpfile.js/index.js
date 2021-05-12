026dee89ed67b8baffc9a96adfc9f8f61c0b0e5f
hisho <hisho.web@gmail.com>
2021-05-13 01:40:48 +0900

    🔧 add generate  tailwind config types

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,12 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp');
+const path = require('path');
 const image = require('./createType');
+const tailwindConfigType = require('./tailwindConfigType');
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 exports.image = image;
+exports.tailwindConfigType = tailwindConfigType;
+
 exports.watch = () => {
   gulp.watch(
     [`src/images/**/*.png`, `src/images/**/*.jpg`, `src/images/**/*.jpeg`],
     image
+  );
+  gulp.watch(
+    [
+      `src/configs/variables.ts`,
+      path.join(process.cwd(), 'tailwind.config.js'),
+      'tailwind/**/*.js',
+    ],
+    tailwindConfigType
   );
 };
